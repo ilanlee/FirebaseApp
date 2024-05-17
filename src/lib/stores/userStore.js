d0d91fc9148776275c68e01fb533a2d9850bd347
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-05-17 17:46:35 -0400

    First time working

--- src/lib/stores/userStore.js
+++ src/lib/stores/userStore.js
@@ -1,8 +1,12 @@
 import { writable } from "svelte/store";
-import { onAuthStateChanged } from "@firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "$lib/firebase.js";
 
 export const user = writable(null);
 
-onAuthStateChanged(user => {
-  user.set(user);
-});
+// Check if running in the browser
+if (typeof window !== 'undefined') {
+  onAuthStateChanged(auth, (user) => {
+    user.set(user);
+  });
+}

f8a8c4164225f347ed47ff7450d5f5379db4ff78
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-05-18 17:58:57 -0400

    A working upload with a form when successfully uploaded

--- src/lib/components/ImageForm.svelte
+++ src/lib/components/ImageForm.svelte
@@ -1,16 +1,7 @@
-<script>
-    import { onMount } from 'svelte';
-  
-    export let imageName;
-    export let imageURL;
+<script> 
+    export let imageName = '';
+    export let imageURL = '';
     let imageDescription = '';
-  
-    onMount(() => {
-      // Resize the image to match the form fields height
-      const image = document.querySelector('.image');
-      const formFields = document.querySelector('.form-fields');
-      image.style.height = `${formFields.offsetHeight}px`;
-    });
   </script>
   
   <div class="flex items-center">
@@ -20,13 +11,13 @@
   
     <div class="form-fields ml-4">
       <label for="imageName">Image Name:</label>
-      <input type="text" id="imageName" bind:value={imageName} />
+      <input type="text" id="imageName" placeholder={imageName} bind:value={imageName} />
   
       <label for="imageURL">Image URL:</label>
-      <input type="text" id="imageURL" bind:value={imageURL} />
+      <input type="text" id="imageURL" bind:value={imageURL} disabled readonly />
   
       <label for="imageDescription">Image Description:</label>
-      <textarea id="imageDescription" bind:value={imageDescription} />
+      <textarea id="imageDescription" placeholder="Image description" bind:value={imageDescription} />
     </div>
   </div>
   
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,12 @@
   import LogoutButton from "$lib/components/LogoutButton.svelte"; 
   import ImageUpload from "../lib/components/ImageUpload.svelte";
   import ImageForm from "../lib/components/ImageForm.svelte";
+  import { imageStore } from "$lib/stores/ImageUploadStore.js";
+
+  let uploadedImages = [];
+    imageStore.subscribe((value) => {
+      uploadedImages = value;
+        });
 
   let email = '';
   let password = '';
@@ -14,7 +20,13 @@
 {#if $user}
   <p>Welcome, {$user.email}!</p>
   <ImageUpload />
-  <ImageForm />
+    <div>
+      <ul>
+        {#each uploadedImages as image}
+            <ImageForm imageName={image.name} imageURL={image.url} />
+        {/each}
+      </ul>
+    </div>
   <LogoutButton />
 {:else}
   <h2 class="underline">Login</h2>

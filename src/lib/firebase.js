8bb4c9a988b6467856f6d82611aed4502dadd1f1
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-05-17 20:01:37 -0400

    Working login with an error at the start

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,10 +1,11 @@
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, doc, setDoc } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASEURL,
   projectId: import.meta.env.VITE_PROJECTID,
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
--- src/lib/stores/userStore.js
+++ src/lib/stores/userStore.js
@@ -4,11 +4,6 @@ import { auth } from "$lib/firebase.js";
 
 export const user = writable(null);
 
-// Check if running in the browser
-if (typeof window !== 'undefined') {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      user.set(user);
-    }
-  });
-}
+onAuthStateChanged(auth, (firebaseUser) => { // Rename to firebaseUser for clarity
+  user.set(firebaseUser);  // Update the Svelte store
+});

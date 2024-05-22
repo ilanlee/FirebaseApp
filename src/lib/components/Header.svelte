e6d38cf7f62ef62dd2d80d0ab8bcdb8192157bc2
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-05-22 17:54:24 -0400

    Sign out is not working

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,18 +1,10 @@
 <script>
     import Logo from "./Logo.svelte";
-    import { signOut } from 'firebase/auth';
-    async function logOut() {
-		await fetch('/', {
-			method: 'DELETE'
-		});
-		await signOut(auth);
-		goto('/');
-	}
 </script>
 
 <header class="sticky top-0 left-0 flex items-center justify-between">
     <Logo/>
     <div class="flex gap-5 items-center">
-        <button on:click={logOut} variant="ghost" class="">Sign out</button>
+        <button variant="ghost" class="">Sign out</button>
     </div>
 </header>

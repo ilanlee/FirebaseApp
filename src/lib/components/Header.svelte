ac016d19b01309badbb6ddabb323e06c12292f86
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-05-25 13:08:57 -0400

    Logout button is working

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,22 @@
 <script>
-    import Logo from "./Logo.svelte";
+  import Logo from "./Logo.svelte";
+  import { auth } from "$lib/firebase.js";
+  import { signOut } from "firebase/auth"; 
+
+  async function logOut() {
+    try {
+        await signOut(auth);
+            window.location.href = '/';
+            console.log('User signed out!'); 
+        } catch (error) {
+            console.error("Error signing out:", error);
+    }
+  }
 </script>
 
 <header class="sticky top-0 left-0 flex items-center justify-between">
     <Logo/>
     <div class="flex gap-5 items-center">
-        <button variant="ghost" class="">Sign out</button>
+        <button on:click={logOut} variant="ghost" class="button">Sign out</button>
     </div>
 </header>
